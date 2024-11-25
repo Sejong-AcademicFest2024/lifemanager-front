5ecab32f782c094bee821cdee3b10fd363df7f07
Doyeon-Park <eowjs2461@gmail.com>
2024-11-25 13:17:29 +0900

    랜딩페이지 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Router from "./Router";
+import { useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const baseWidth = 390; // 기준 너비
+    const baseFontSize = 16; // 기준 폰트 크기
+    function handleResize() {
+      const scaleFactor = window.innerWidth / baseWidth;
+      const newFontSize = baseFontSize * scaleFactor;
+      document.documentElement.style.fontSize = `${newFontSize}px`;
+    }
+    window.addEventListener("resize", handleResize);
+    // 컴포넌트 마운트 시 초기 폰트 크기 설정
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return <Router />;
 }
-
 export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+const Landing = () => {
+  return <TotalComponent>랜딩페이지</TotalComponent>;
+};
+export default Landing;
+const TotalComponent = styled.div``;
